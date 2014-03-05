6bf150bcfe41b739c272b25325db36f3051a288d
Marshal <marshal.wu@gmail.com>
2014-03-05 21:13:11 +0800

    prepare m2

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,30 +6,68 @@ require.config({
 
 require(['can'], function (can) {
     var Info = can.Model({
-        findOne: 'GET /info/{id}'
+        findOne: 'GET /info/{id}',
+        update: 'PUT /info/{id}'
     }, {});
 
-//    can.fixture({
-//        'GET /info/{id}.json': function () {
-//            return {
-//                id: 1,
-//                place: '北京 - 奥利匹克森林公园',
-//                aqi: 283,
-//                desc: "重度污染"
-//            }
-//        }
-//    });
-
-    var InfoWidget = can.Control({
-        init: function () {
-            var el = this.element;
-            Info.findOne({id: 1},function (data) {
-                el.html(can.view('../views/info.ejs', data));
-            },function(xhr){
-                console.log(xhr);
-            });
+    can.fixture({
+        'GET /info/{id}': function () {
+            return {
+                id: 1,
+                place: '奥利匹克森林公园',
+                aqi: '283',
+                desc: "重度污染"
+            }
+        },
+        'PUT /info/{id}': function () {
+            return {
+            };
         }
     });
 
-    var infoWidget = new InfoWidget('#content');
+    //信息显示控件
+    var InfoWidget = can.Control({
+        init: function () {
+            this.render();
+            var self = this;
+            Info.bind('updated', function () {
+                self.render();
+            });
+//            this.options.info.bind('change', function (event, attr, how, newVal, oldVal) {
+//                console.log('>>>>>>>change:'+attr);
+//            });
+
+        },
+        render: function () {
+            this.element.html(can.view('../views/info.ejs', this.options.info));
+        }
+    });
+
+    //表单显示控件
+    var FormWidget = can.Control({
+        init: function (el, info) {
+            this.render();
+            var self = this;
+            Info.bind('updated', function (event,updated) {
+                if(updated.id==self.options.info.id){
+                    self.render();
+                }
+            });
+        },
+        render: function () {
+            this.element.html(can.view('../views/form.ejs', this.options.info));
+        },
+        'button click': function () {
+            var form = this.element.find('input');
+            var values = can.deparam(form.serialize());
+            console.log(values);
+//            this.options.info.attr(values);
+            this.options.info.attr(values).save();
+        }
+    });
+
+    Info.findOne({id: 1}, function (info) {
+        new InfoWidget('#info', {info: info});
+        new FormWidget('#form', {info: info});
+    });
 });
